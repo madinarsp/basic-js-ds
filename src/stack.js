425fb94ae8e7597959cade9cbb28f657cf8ecfb1
madinarsp <madinarsp20@gmail.com>
2022-05-02 23:13:49 +0600

    refactor: add solution of stack from previous season

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let removedElem = this.stack[this.stack.length - 1];
+    if(this.stack.length != 0) this.stack.pop();
+    return removedElem;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
